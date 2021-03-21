8a91af61d5a95bb1a87523d57ecc9f1f5014bc60
Max <78378142+Space-Rocket-Discord@users.noreply.github.com>
2021-03-21 21:12:15 +0100

    Update addrole.js

--- server/addrole.js
+++ server/addrole.js
@@ -6,26 +6,7 @@ module.exports = {
         let member = message.mentions.members.first();
         let askrole = message.content.substring(7);
         let role = message.guild.roles.cache.find(r => r.name === "askrole");
-
-         // If the author don't have perm
-         if(!message.member.hasPermission("MANAGE_ROLES")) return message.channel.send('You can\'t use that!');
-         // If the bot don't have perm
-         if(!message.guild.me.hasPermission("MANAGE_ROLES")) return message.channel.send('I don\'t have the right permissions.');
-         // If the bot don't find the user
-         if(!member) return message.channel.send('Can\'t seem to find this user.');
-         //If he bot dont find the role
-         if(!role) return message.channel.send('Can\'t seem to find this role.');
-
-        const embed = new MessageEmbed()
-        // Set the title of the field
-        .setTitle('Role Added!')
-        // Set the color of the embed
-        .setColor(0xff0000)
-        // Set the main content of the embed
-        .setDescription(`${message.author} added ${role} role to ${member}`);
-      // Send the embed to the same channel as the message
-        message.delete()
-        member.roles.add(role)
-        message.channel.send(embed)      
+        
+        message.channel.send(askrole)
     } 
 };
