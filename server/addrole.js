f12501093d7c395aefb77e813d25f2ad9cbff8bd
Max <78378142+Space-Rocket-Discord@users.noreply.github.com>
2021-03-21 21:37:57 +0100

    Update addrole.js

--- server/addrole.js
+++ server/addrole.js
@@ -4,7 +4,7 @@ module.exports = {
     execute(message) {
         const { Client, MessageEmbed } = require('discord.js');
         let askedrole = message.content.substring(31);
-        let role = message.guild.roles.cache.find(r => r.name === askedrole);
+        let myRole = message.guild.roles.cache.find(role => role.name === askedrole);
         let member = message.mentions.members.first();
         
         // If the author don't have perm
@@ -16,14 +16,14 @@ module.exports = {
         
         const embed = new MessageEmbed()
         // Set the title of the field
-        .setTitle('test')
+        .setTitle('addrole test')
         // Set the color of the embed
         .setColor(0xff0000)
         // Set the main content of the embed
-        .setDescription(`**test**`);
+        .setDescription(`**Did it worked lol**`);
         // Send the embed to the same channel as the message
         message.delete()
-        member.roles.add(role)
+        member.roles.add(myRole)
         message.channel.send(embed)      
     } 
 };
